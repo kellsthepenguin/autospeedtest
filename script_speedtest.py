bb92b65f800324f30ae44611c569bcb24f949dd7
Kim Jioo <jwkim101201@gmail.com>
2021-04-21 09:56:46 +0900

    1th todo completed

--- script_speedtest.py
+++ script_speedtest.py
@@ -1,25 +1,37 @@
-from speedtest import Speedtest
 import yaml
+import math
+from speedtest import Speedtest
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-helper = Speedtest()
 
-helper.get_best_server()
-helper.download()
-helper.upload()
+def job():
+    helper = Speedtest()
 
-result = helper.results.dict()
+    helper.get_best_server()
+    helper.download()
+    helper.upload()
 
-download = float(result.get('download'))
-upload = float(result.get('upload'))
-f = open('./config.yml', 'r')
+    result = helper.results.dict()
 
-data = yaml.load(f.read(), Loader=yaml.FullLoader)
-originalSpeed = data.get('originalSpeed')
+    download = float(result.get('download'))
+    upload = float(result.get('upload'))
+    f = open('./config.yml', 'r')
 
-if download < (originalSpeed / 4) or upload < (originalSpeed / 4):
-    print('아니 25%도 안나온다고요 ㅁㅊ')
+    data = yaml.load(f.read(), Loader=yaml.FullLoader)
+    originalSpeed = data.get('originalSpeed')
 
-print(download)
-print(upload)
+    mbDownload = math.floor(download / 1e+6)
+    mbUpload = math.floor(upload / 1e+6)
 
-f.close()
+    if download < (originalSpeed / 4) or upload < (originalSpeed / 4):
+        print(f'다운로드: {mbDownload}Mbps 업로드: {mbUpload}Mbps 아니 25%도 안나온다고요 ㅁㅊ')
+
+    print(f'다운로드: {mbDownload}Mbps 업로드: {mbUpload}Mbps')
+
+    f.close()
+
+
+scheduler = BlockingScheduler()
+
+scheduler.add_job(job, 'interval', minutes=5)
+scheduler.start()
